7faaf55f9c418d4371cc10e202dc4563950af17d
Milos89J <91075244+Milos89J@users.noreply.github.com>
2022-11-22 20:51:11 +0100

    added new page 'PostBlogView'
    create few simple function and two new buttons

---
+++ src/components/PostBlogView.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="blog-wrapper">
+    <div class="blog-content">
+      <div>
+       <h2 v-if="blog.welcome">{{ blog.title }}</h2>
+       <h2 v-else>{{ blog.title }}</h2>
+       <p v-if="blog.welcome">{{blog.postBlog}}</p>
+       <p class="content-preview" v-else>{{blog.blogHTML}}</p>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "postBlog",
+    props: ["blog"],
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="home">
-  </div>
+   <PostBlogView :blog="welcome"/>
+   <PostBlogView :blog="blog" v-for="(blog, index) in sample" :key="index"/>
+ </div>
 </template>
 
 <script>
+import PostBlogView from '../components/PostBlogView.vue';
 export default {
-  name: "Home",
-  components: {},
+ name: "Home",
+ components: { PostBlogView },
+ data() {
+   return {
+     welcome: {
+       title: "Welcome",
+       postBlog: "The following is a custom example and tutorial on how to setup a simple login page using Vue.js + Vuex and JWT authentication.",
+       welcome: true,
+       photo: "coding",
+     },
+     sample: [
+       {
+         title: "While this",
+         blogHTML: "While this does merge in the PR into the master branch",
+         blogPhoto: "beautiful-stories",
+       },
+       {
+         title: "While this",
+         blogHTML: "While this does merge in the PR into the master branch",
+         blogPhoto: "designed-for-everything",
+       },
+     ]
+   }
+ }
 };
 </script>
+
+<style lang="scss" scoped>
+
+</style>
